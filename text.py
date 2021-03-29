92df163195a2ce4e3e4e7bb8858e3c4e7c376f7b
nadaverez56 <nadaverez56@gmail.com>
2021-03-29 13:40:28 +0300

    Version 1.0

---
+++ text.py
@@ -0,0 +1,17 @@
+def countword() -> int:
+    file = open('text.txt')
+    text = file.read()
+    list_of_words=text.split()
+    word = list_of_words[0]
+    counter=0
+    for i in range(1,len(list_of_words)):        
+        rev=revword(list_of_words[i])
+        if rev == word:
+            counter+=1
+    return counter+1     
+
+def revword(word:str) -> str:
+    st = ''
+    for letter in range(0,len(word)):
+        st = word[letter] + st
+    return st
